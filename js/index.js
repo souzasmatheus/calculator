707219b13681454876621f9bbe7e22526fa6684e
Matheus Souza <matheu.sk.9@hotmail.com>
2019-02-06 16:26:49 -0200

    Handle trigonometry before another operation

--- js/index.js
+++ js/index.js
@@ -8,13 +8,15 @@
 */
 
 // Variables
-const buttons = document.querySelectorAll('.key')
+const numbers = document.querySelectorAll('.num-key')
+const operands = document.querySelectorAll('.operand-key')
 const enter = document.querySelector('.key-enter')
 const sciKeys = document.querySelectorAll('.sci-key')
 
 let string = ''
 let sciMode = false
 let sciType = ''
+let sciString = ''
 
 // General functions
 
@@ -36,13 +38,23 @@ const handleScience = (type) => (num) => {
 
 // Specific functions
 
-buttons.forEach((button) => button.addEventListener('click', () => {
+numbers.forEach((number) => number.addEventListener('click', () => {
     if (sciMode === false) {
-        string += button.innerHTML
+        string += number.innerText
     } else {
-        handleScience(sciType)(Number(button.innerText))
+        sciString += number.innerText
+    }
+}))
+
+operands.forEach((operand) => operand.addEventListener('click', () => {
+    if (sciMode === false) {
+        string += operand.innerText
+        console.log(string)
+    } else {
+        handleScience(sciType)(Number(sciString))
         sciMode = false
         sciType = ''
+        string += operand.innerText
     }
 }))
 
@@ -53,4 +65,5 @@ enter.addEventListener('click', () => {
 sciKeys.forEach((key) => key.addEventListener('click', () => {
     sciMode = true
     sciType = key.innerText
+    console.log(sciType)
 }))
